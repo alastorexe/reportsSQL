2dbc974bf5f876a286bc39a7d354a9c2217a132e
Alex Vokh <izrail1233@gmail.com>
2021-01-11 11:37:11 +0300

    add import nomenclature

--- importNomenclature.php
+++ importNomenclature.php
@@ -232,304 +232,102 @@ function _parseXML($xmlObject)
     return $xmlArray;
 }
 
-$clientID = 4;
-$fileXML = "fileXML.xml";
 
-if (false) {
+/*
+ "client_id"    => $clientID,
+ "name"         => $categoryName,
+ "parent"       => $categoryParent,
+ "uid"          => $categoryUID,
+ "is_activated" => 1,
+ "parent_uid"   => $categoryParentUID,
+ "goods"        => [],
+*/
 
-    $fullLoad = false;
-    $PartLoad = false;
-    $JSONLoad = false;
-    $parseXML = false;
+function createTree($categories)
+{
+    $tree = [];
 
-    if ($fullLoad) {
-        echo "start\n";
-        $xml = simplexml_load_file($fileXML);
-        if ($xml === false) {
-            throw new OrlanException("Не удалось разобрать файл");
+    foreach ($categories as $category) {
+        if (empty($category["parent_uid"])) {
+            $tree[] = $category;
+            unset($category);
         }
-        echo "load complete\n";
-        echo "parse start\n";
-        //$answer = _parseXML($xml);
-        echo "parse finish\n";
-        echo "parse json start\n";
-        $json = json_encode($xml, JSON_UNESCAPED_UNICODE);
-        echo "parse json finish\n";
-        echo "file start\n";
-        file_put_contents("JSON_data.json", $json, FILE_APPEND);
-        echo "file finish\n";
     }
 
-    if ($PartLoad) {
-        $xml = new XMLReader();
-        $xml->open($fileXML);
-
-        $tag = "Объект";
-        //$tag = "ПравилаОбмена";
-
-        while ($xml->read() && $xml->name != $tag) {
-            ;
-        }
-
-        while ($xml->name == $tag) {
-            $element = new SimpleXMLElement($xml->readOuterXML());
-
-            $json = json_encode($element, JSON_UNESCAPED_UNICODE);
-            $json = substr($json, 1);
-            $json = substr($json, 0, -1);
-            $json = '"' . "{$tag}" . '"' . " : {" . $json . "},";
-
-            file_put_contents("JSON_data.json", $json, FILE_APPEND);
-
-            $xml->next($tag);
-            unset($element);
-        }
-
-        $xml->close();
-    }
-
-    if ($JSONLoad) {
-        echo "start\n";
-        $data = file_get_contents($fileJSON);
-        echo "load complete\n";
-        echo "parse start\n";
-        $json = json_decode($data, JSON_UNESCAPED_UNICODE);
-        echo "parse finish\n";
-    }
-
-    if ($parseXML) {
-        $xml = new XMLReader();
-        $xml->open($fileXML);
-
-        $tag = "Объект";
-        //$tag = "ПравилаОбмена";
-
-        while ($xml->read() && $xml->name != $tag) {
-            ;
-        }
-
-        while ($xml->name == $tag) {
-            $element = new SimpleXMLElement($xml->readOuterXML());
-
-            $attributes = $element->attributes();
-
-            /*
-                print_r("parse _xml start\n");
-                $dataXML = _parseXML($element);
-                print_r("parse _xml finish\n");
-
-                $typeData = $dataXML["Объект"]["_attributes_"];
-
-                $type = $typeData["Тип"];*/
-
-            $type = $attributes["Тип"];
-
-            if ($type == "СправочникСсылка.Номенклатура") {
-
-                $good = ORM::forTable("goods")->create();
-                $goodsCategory = ORM::forTable("goods_categories")->create();
-
-                /* $goodUID = $dataXML["Объект"]["value"]["Ссылка"]["value"][0]["value"]["Свойство"]["value"][0]["value"]["Значение"]["value"];
-                 $goodCode = $dataXML["Объект"]["value"]["Ссылка"]["value"][0]["value"]["Свойство"]["value"][1]["value"]["Значение"]["value"];
-                 $goodName = $dataXML["Объект"]["value"]["Ссылка"]["value"][6]["value"]["Значение"]["value"];
-
-                 $categoryUID = $dataXML["Объект"]["value"]["Ссылка"]["value"][2]["value"]["Ссылка"]["value"]["Свойство"]["value"][0]["value"]["Значение"]["value"];
-                 $categoryName = $dataXML["Объект"]["value"]["Ссылка"]["value"][2]["value"]["Ссылка"]["value"]["Свойство"]["value"][1]["value"]["Значение"]["value"];
-
-                 print_r($goodUID);
+    // уровень вложенности 2
+    foreach ($tree as &$itemTree) {
+        foreach ($categories as $category) {
+            /* if(!empty($category["parent_uid"])) {
+                 print_r($itemTree);
+                 print_r($category);
                  echo "\n";
-                 print_r($goodCode);
-                 echo "\n";
-                 print_r($goodName);
-                 echo "\n";
-                 print_r($categoryUID);
-                 echo "\n";
-                 print_r($categoryName);
-                 echo "\n";*/
-
-
-                $goodUID = $element->Ссылка->Свойство[0]->Значение;
-                $goodCode = $element->Ссылка->Свойство[1]->Значение;
-                $goodArticle = $element->Свойство[0]->Значение ?? '';
-                $goodName = $element->Свойство[3]->Значение ?? '';
-                $goodDescription = $element->Свойство[4]->Значение ?? '';
-
-                if ($goodDescription) {
-                    print_r($element);
-                    exit();
-                }
-
-                /*
-                 * Cсылка
-                 *      Cвойство[0] - goodUID
-                 *      Cвойство[1] - goodCode
-                 *      Свойство[2] - Принадлежность какой-то группе (-)
-                   Свойство
-                        [0] - Артикул товара
-                        [1] - Ссылка на категорию товара
-                            Свойство[0] - categoryUID
-                            Cвойство[1] - categoryName
-                            Cвойство[2] - Принадлежность какой-то группе (-)
-                        [2] - Единица измерения товара
-                            Свойство[0] - uid единицы
-                            Cвойство[1] - код единицы измерения
-                        [3] - Полное наименование товара (goodName)
-                        [4] - Описание товара
-                        [5] - Cтавка НДС (-)
-                        [6] - Наименование категории товара
-                        [7] - Сокращённое наименование товара (-)
-                        [8] - Пометка для удаления (-)
-                        [9] - Информация о родителе чего-то
-                             Свойство[0] - parentUID
-                             Свойство[1] - parentCode
-                             Cвойство[2] - Принадлежность какой-то группе (-)
-                        [10] - Автоматическая скидка?? (Число) (-)
-                        [11] - Cхема скидок?? (Число) (-)
-
-                    */
-
-
-                $categoryUID = $element->Свойство[1]->Ссылка->Свойство[0]->Значение;
-                $categoryName = $element->Свойство[1]->Ссылка->Свойство[1]->Значение;
-                $categoryParentUID = $element->Свойство[7]->Ссылка->Свойство[0]->Значение ?? '';
-                $categoryParent = $element->Свойство[7]->Ссылка->Свойство[1]->Значение ?? 0;
-
-                print_r($goodUID);
-                echo "\n";
-                print_r($goodCode);
-                echo "\n";
-                print_r($goodArticle);
-                echo "\n";
-                print_r($goodName);
-                echo "\n";
-                print_r($goodDescription);
-                echo "\n";
-                print_r($categoryUID);
-                echo "\n";
-                print_r($categoryName);
-                echo "\n";
-                print_r($categoryParentUID);
-                echo "\n";
-                print_r($categoryParent);
-                echo "\n";
-
-                /*$goodsCategory->set([
-                    "client_id" => $clientID,
-                    "name" => $categoryName,
-                    "parent" => $categoryParent,
-                    "description" => "Категория " . $categoryName,
-                    "uid" => $categoryUID,
-                    "is_activated" => 1,
-                    "parent_uid" => $categoryParentUID
-                ]);
-                $goodsCategory->save();
-                if(empty($goodsCategory)) {
-                    throw new OrlanException("Ошибка сохранения категории товара");
-                }
-
-                $good->set([
-                    "client_id" => $clientID,
-                    "user_id"   => 1,
-                    "category_id"   => 1,
-                    "code"   => $goodCode,
-                    "name"   => $goodName,
-                    "article"   => $goodArticle,
-                    "price"   => 1,
-                    "description"   => $goodDescription,
-                    "price_out"   => 1
-                ]);
-                $good->save();
-                if (empty($good)) {
-                    throw new OrlanException("Ошибка сохранения товара");
-                }*/
+                 echo "tree:\n";
+                 print_r($itemTree["uid"]);
+                 echo "\n category";
+                 print_r($category["parent_uid"]);
+                 exit();
+             }*/
+            if ($itemTree["uid"] == $category["parent_uid"]) {
+                $itemTree["categories"] = $category;
+                unset($category);
             }
-
-            if (false) {
-                $types = [
-                    "СправочникСсылка.Номенклатура",
-                    "СправочникСсылка.ВидыНоменклатуры",
-                    "СправочникСсылка.УпаковкиНоменклатуры",
-                    "CправочникСсылка.ХарактеристикиНоменклатуры",
-                    "CправочникСсылка.СегментыНоменклатуры",
-                    "СправочникСсылка.СерииНоменклатуры",
-                    "СправочникСсылка.НоменклатураПрисоединённыеФайлы",
-                ];
-
-                unset($typeData);
-                unset($dataXML);
-
-                if (in_array($type, $types)) {
-                    print_r("json ecncode start\n");
-                    $json = json_encode($element, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-                    $json = substr($json, 1);
-                    $json = substr($json, 0, -1);
-                    $json = '"' . "{$tag}" . '"' . " : {" . $json . "},\n";
-                    print_r("json ecncode finish\n");
-
-                    switch ($type) {
-                        case "СправочникСсылка.Номенклатура": {
-                            file_put_contents("nomenclature_data.json", $json, FILE_APPEND);
-                        }
-                            break;
-                        case "СправочникСсылка.ВидыНоменклатуры" : {
-                            file_put_contents("type_nomenclature_data.json", $json, FILE_APPEND);
-                        }
-                            break;
-
-                        case "СправочникСсылка.УпаковкиНоменклатуры" : {
-                            file_put_contents("packaging_nomenclature_data.json", $json, FILE_APPEND);
-                        }
-                            break;
-
-                        case "CправочникСсылка.ХарактеристикиНоменклатуры" : {
-                            file_put_contents("specifications_nomenclature_data.json", $json, FILE_APPEND);
-                        }
-                            break;
-
-                        case "CправочникСсылка.СегментыНоменклатуры" : {
-                            file_put_contents("segments_nomenclature_data.json", $json, FILE_APPEND);
-                        }
-                            break;
-
-                        case "СправочникСсылка.СерииНоменклатуры" : {
-                            file_put_contents("series_nomenclature_data.json", $json, FILE_APPEND);
-                        }
-                            break;
-                        case "СправочникСсылка.НоменклатураПрисоединённыеФайлы" : {
-                            file_put_contents("files_nomenclature_data.json", $json, FILE_APPEND);
-                        }
-                            break;
-                    }
-
-                    /*  print_r("ТИП:");
-                      print_r($typeData);
-
-                      print_r("\n\n");
-
-                      print_r($dataXML);
-                      print_r("\n\n");*/
-
-                    /*$json = json_encode($element, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
-                    file_put_contents("json_data.json", $json, FILE_APPEND);*/
-                }
-
-            }
-
-            $xml->next($tag);
-            unset($element);
         }
-
-        $fileContent = file_get_contents($fileNomenclature);
-        $fileContent = substr($fileContent, 0, -1);
-
-        file_put_contents($fileNomenclature, "{\n" . $fileContent);
-        file_put_contents($fileNomenclature, "\n}", FILE_APPEND);
-
-        $xml->close();
     }
+
+    return $tree;
 }
 
-$clientID = 780;
+function buildTree(&$categories, $parentUID = '')
+{
+    $tree = [];
+
+    foreach ($categories as &$category) {
+        if ($category["parent_uid"] == $parentUID) {
+            $children = buildTree($categories, $category["uid"]);
+            if ($children) {
+                $category["categories"] = $children;
+            }
+            //$tree[$category["uid"]] = $category;
+            $tree[] = $category;
+            unset($category);
+        }
+    }
+
+    return $tree;
+}
+
+function makeTree(&$categories, $parentUID = '')
+{
+    // будем строить новый массив-дерево
+    $tree = [];
+    foreach ($categories as $key => $category) {
+        /* проверяем, относится ли родитель элемента к самому
+        верхнему уровню и не ссылается ли на самого себя */
+        if ($category["parent_uid"] == $parentUID && $category["uid"] != $category["parent_uid"]) {
+            // удаляем этот элемент из общего массива
+            unset($categories[$key]);
+            $tree[] = [
+                // однако сохраним его в дереве
+                $key         => $category,
+                /* пробежим еще раз, но с уже
+                меньшим числом элементов */
+                "categories" => makeTree($items, $category["uid"]),
+            ];
+        }
+    }
+
+    return $tree;
+}
+
+/*$data = '{"goods": [{"uid": "4b7f754f-0b80-48e7-88d3-d385ee1eff29", "code": "6", "name": "тетрадь студенческая 64 листа", "price": 34, "parent": "", "price_out": 34, "description": ""}, {"uid": "515bc2ea-9cc6-48eb-8f83-eef1acd552c6", "name": "теефон", "price": 1000, "parent": "", "price_out": 1000, "description": ""}, {"uid": "0357cb2d-3321-4431-8a22-9d63b1e0894f", "code": "7", "name": "Булка с мясом", "price": 500, "parent": "", "price_out": 500, "description": ""}, {"uid": "f27004b4-b169-4249-ad0c-d08c5bc87d65", "code": "5", "name": "Карандаш простой ББ", "price": 6, "parent": "", "price_out": 6, "description": ""}, {"uid": "fff7e36f-7322-41a2-989b-7db65a1f0871", "code": "4", "name": "ручка красная гелевая", "price": 40, "parent": "", "price_out": 40, "description": ""}, {"uid": "0f5f1431-e53c-47f0-8e28-4225ca7c0717", "code": "22", "name": "!мой большой товар", "price": 1000, "parent": "", "price_out": 1000, "description": ""}, {"uid": "51801880-1fb0-4844-b996-e9d5594f14c4", "code": "23", "name": "!весовой товар", "price": 1000, "parent": "", "price_out": 1000, "description": ""}, {"uid": "cdc7c278-09a2-4731-b42c-c1277313246e", "code": "3", "name": "карта обыкновенная", "price": 400, "parent": "", "price_out": 400, "description": ""}, {"uid": "0e853c3a-94b5-414c-850c-8977267f10f4", "code": "1", "name": "тестовый товар 1", "price": 800, "parent": "", "price_out": 800, "description": ""}], "categories": [{"uid": "d962ada6-ad7e-4591-a1d8-c38f5d4bda86", "code": "9", "name": "группа2", "goods": [{"uid": "73448bf2-fb21-4489-8910-427729324265", "code": "17", "name": "второй товар группы 2", "price": 2, "parent": "d962ada6-ad7e-4591-a1d8-c38f5d4bda86", "price_out": 2, "description": ""}, {"uid": "81640528-b7a8-470e-a4ec-1d66482eed39", "code": "16", "name": "товар группы 2", "price": 22, "parent": "d962ada6-ad7e-4591-a1d8-c38f5d4bda86", "price_out": 22, "description": ""}], "parent": "", "categories": [{"uid": "e4165804-f064-4ed5-bc64-d38248236334", "code": "11", "name": "подгруппа2", "goods": [{"uid": "2a296e0c-12d6-477c-a4fe-6dcf95bf8d0b", "code": "15", "name": "второй товар подгруппы 2", "price": 2, "parent": "e4165804-f064-4ed5-bc64-d38248236334", "price_out": 2, "description": ""}, {"uid": "2ad8a2a8-b1e7-4621-bb97-8ab56d2a44a0", "code": "14", "name": "товар подгруппы 2", "price": 22, "parent": "e4165804-f064-4ed5-bc64-d38248236334", "price_out": 22, "description": ""}], "parent": "d962ada6-ad7e-4591-a1d8-c38f5d4bda86", "categories": [{"uid": "684dc76f-9291-4d51-a834-5cad75fcec0b", "code": "18", "name": "подгруппа подгруппы2", "goods": [{"uid": "2291bc0a-b863-4e5e-8b02-a2f2051c85bd", "code": "21", "name": "еще один самый последний в иерархии товар", "price": 0, "parent": "684dc76f-9291-4d51-a834-5cad75fcec0b", "price_out": 0, "description": ""}, {"uid": "9450cb36-a7cb-4927-890e-e7d4c9aa35e7", "code": "19", "name": "последний в иерархии товар", "price": 0, "parent": "684dc76f-9291-4d51-a834-5cad75fcec0b", "price_out": 0, "description": ""}], "parent": "e4165804-f064-4ed5-bc64-d38248236334", "categories": [], "description": "Категория подгруппа подгруппы2"}], "description": "Категория подгруппа2"}], "description": "Категория группа2"}, {"uid": "00dd7bf5-204a-450c-a451-3ac89181c05e", "code": "8", "name": "группа 1", "goods": [{"uid": "454d1283-0484-46b9-9434-2d2a28ca01d8", "code": "12", "name": "товар группы1", "price": 1, "parent": "00dd7bf5-204a-450c-a451-3ac89181c05e", "price_out": 1, "description": ""}], "parent": "", "categories": [{"uid": "46327fba-0b18-4b55-888a-8259a98c7c7d", "code": "20", "name": "еще одна подгруппа1", "goods": [], "parent": "00dd7bf5-204a-450c-a451-3ac89181c05e", "categories": [], "description": "Категория еще одна подгруппа1"}, {"uid": "75814b8a-1c4c-49d2-8247-bf85aa592a52", "code": "10", "name": "подгруппа1", "goods": [{"uid": "05affe2f-8c46-4da0-a9d1-f1f8ea6221c8", "code": "13", "name": "товар подгруппы 1", "price": 11, "parent": "75814b8a-1c4c-49d2-8247-bf85aa592a52", "price_out": 11, "description": ""}], "parent": "00dd7bf5-204a-450c-a451-3ac89181c05e", "categories": [], "description": "Категория подгруппа1"}], "description": "Категория группа 1"}]}';
+
+$json = json_decode($data,  JSON_PRETTY_PRINT);
+
+print_r($json);
+exit();*/
+
+// 780
+$clientID = 4;
 $fileXML = "fileXML.xml";
 
 $goodsCategory = ORM::forTable("goods_categories")
@@ -643,6 +441,7 @@ while ($xml->name == $tag) {
             }
         }
 
+        // если категория не существует
         if ($flag) {
             $category = [
                 "client_id"    => $clientID,
@@ -727,20 +526,21 @@ while ($xml->name == $tag) {
 
 $xml->close();
 
-/*foreach ($categories as &$category) {
+foreach ($categories as &$category) {
     if (!empty($goods[$category["uid"]])) {
         $category["goods"] = $goods[$category["uid"]];
         unset($goods[$category["uid"]]);
     }
-}*/
+}
+
+print_r("Товары в категориях\n");
 
 // построить дерево категорий
 $treeCategories = buildTree($categories);
 //$treeCategories = makeTree($categories);
 //$treeCategories = createTree($categories);
 
-print_r($treeCategories);
-exit();
+print_r("Построили дерево категорий\n");
 
 $data = [
     "categories" => $treeCategories,
